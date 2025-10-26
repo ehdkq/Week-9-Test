9f3aba28f7542e0a1faff4e4e769bcb92520fb65
madelinebro <mbxkd@umsystem.edu>
2025-10-26 16:52:51 -0500

    Update hardhat.config.ts

--- deploy-contract/hardhat.config.ts
+++ deploy-contract/hardhat.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const RPC_URL = process.env.RPC_URL!;
-const CHAIN_ID = Number(process.env.CHAIN_ID || "31338");
+const RPC_URL = "https://eth.didlab.org/";
+const CHAIN_ID = 252501;
 const PRIVKEY = process.env.PRIVKEY!;
 
 const config: HardhatUserConfig = {
